7dfc91587ab4147d371f030940de2f8167bd56c1
Thiago Ribeiro <72639507+thiagothr@users.noreply.github.com>
2022-03-07 16:03:23 -0300

    Add files via upload

---
+++ Banco_de_Games.sql
@@ -0,0 +1,42 @@
+
+
+CREATE TABLE Games (
+Nome VARCHAR (200) NOT NULL,
+Desenvolvedora VARCHAR (200) NOT NULL,
+Plataformas VARCHAR (200) NOT NULL,
+Ano_de_lançamento DATE);
+
+
+
+INSERT INTO Games (Nome,Desenvolvedora,Plataformas,Ano_de_lançamento)
+VALUES
+('The last of us part 2', 'Naughty Dog', 'PS4/PS5','19-06-2020'),
+('It Takes Two', 'Hazelight Studios', 'PS4/Xbox One/PS5/Xbox Series X,/Microsoft Windows','26-03-2021'),
+('Resident Evil Village', 'Capcom', 'PS4/Microsoft Windows/Xbox One/Xbox Series X/PS5/Google Stadia', '07-05-2021'),
+('Spider Man Miles Morales', 'Insomniac Games', 'PS4/PS5','12-11-2020'),
+('Elden Ring', 'FromSoftware', 'PS4/PS5/Xbox Series X/Xbox One/Microsoft Windows','25-02-2022'),
+('Doom Eternal', 'id Software/Panic Button Games', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X/Google Stadia','20-03-2020'),
+('Deathloop', 'Arkane Studios', 'PS5/Microsoft Windows','14-09-2021'),
+('Assasins Creed Valhalla', 'Ubisoft', 'PS4/Microsoft Windows/Xbox One/Xbox Series X/PS5/Google Stadia','10-11-2020'),
+('Sifu', 'Sloclap', 'PS4/PS5/Microsoft Windows','06-02-2022'),
+('Kena Bridge Of Spirits', 'Ember Lab', 'PS4/PS5/Microsoft Windows','21-09-2021'),
+('Crash Bandicoot 4', 'Toys for Bob', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X','16-09-2020'),
+('Marvel Guardians of the Galaxy', 'Eidos Montréal', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X','26-10-2021'),
+('Horizon Forbidden West', 'Guerrilla Games', 'PS4/PS5','18-02-2022'),
+('Dying Light 2 Stay Human', 'Techland', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X','03-02-2022'),
+('Cyberpunk', 'CD Projekt RED', 'PS4/Microsoft Windows/Xbox One/Xbox Series X/PS5/Google Stadia','19-11-2020'),
+('Lego Star Wars: The Skywalker Saga', 'TT Games', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X/macOS','05-04-2022'),
+('Pokemon Legens Arceus', 'Nintendo', 'Nintendo Switch','28-01-2021'),
+('Ratchet & Clank Rift Apart', 'Insomniac Games', 'PS5','11-06-2021'),
+('Final Fantasy VII Remake', 'Square Enix', 'PS4/PS5/Microsoft Windows','10-04-2020'),
+('Battlefield 2042', 'DICE', 'PS4/Microsoft Windows/Xbox One/Xbox Series X/PS5','08-10-2021'),
+('Gran Turismo 7', 'Polyphony Digital', 'PS4/PS5','04-03-2022'),
+('Psychonauts 2', 'Double Fine', 'PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X/macOS/Linux','25-08-2021'),
+('Ghost of Tshushima', 'Sucker Punch Productions', 'PS4/PS5','17-07-2020'),
+('S.T.A.L.K.E.R. 2', 'GSC Game World', 'Xbox Series X/Microsoft Windows','08-12-2022'),
+('Genshin Impact', 'miHoYo', 'PS4/PS5/Android/Microsoft Windows/iOS','28-09-2020'),
+('Hitman 3', 'IO Interactive', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X/Google Stadia','20-01-2021'),
+('Starfield', 'Bethesda Game Studios', 'Xbox Series X/Microsoft Window','11-11-2022'),
+('Fall Guys', 'Mediatonic', 'Nintendo Switch/PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X','03-08-2020'),
+('Far Cry 6', 'Ubisoft', 'PS4/PS5/Microsoft Windows/Xbox One/Xbox Series X/Google Stadia/Amazon Luna','06-10-2021'),
+('God of War Ragnarok', 'Santa Monica Studio', 'PS4/PS5','2022');
